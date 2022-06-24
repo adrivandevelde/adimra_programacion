78d59fa6fc62d30e9479405adbd9f60edd51de36
adrivandevelde <vandevelde628@gmail.com>
2022-06-23 21:40:32 -0300

    Cambios

--- semana_17/trabajo_final.py
+++ semana_17/trabajo_final.py
@@ -17,69 +17,9 @@ def solicitar_stock(ruta):
 
 
 def mostrar_stock(datos):
-    print("Producto id: ", datos["productos"][0]["id"])
-    print("Descripción: ", datos["productos"][0]["nombre"])
-    print("Precio unitario: ", datos["productos"][0]["precio"])
-    print("Cantidad disponible: ", datos["productos"][0]["stock"])
-    print("\n")
-    print("Producto id: ", datos["productos"][1]["id"])
-    print("Descripción: ", datos["productos"][1]["nombre"])
-    print("Precio unitario: ", datos["productos"][1]["precio"])
-    print("Cantidad disponible: ", datos["productos"][1]["stock"])
-    print("\n")
-    print("Producto id: ", datos["productos"][2]["id"])
-    print("Descripción: ", datos["productos"][2]["nombre"])
-    print("Precio unitario: ", datos["productos"][2]["precio"])
-    print("Cantidad disponible: ", datos["productos"][2]["stock"])
-    print("\n")
-    print("Producto id: ", datos["productos"][2]["id"])
-    print("Descripción: ", datos["productos"][2]["nombre"])
-    print("Precio unitario: ", datos["productos"][2]["precio"])
-    print("Cantidad disponible: ", datos["productos"][2]["stock"])
-    print("\n")
-    print("Producto id: ", datos["productos"][3]["id"])
-    print("Descripción: ", datos["productos"][3]["nombre"])
-    print("Precio unitario: ", datos["productos"][3]["precio"])
-    print("Cantidad disponible: ", datos["productos"][3]["stock"])
-    print("\n")
-    print("Producto id: ", datos["productos"][4]["id"])
-    print("Descripción: ", datos["productos"][4]["nombre"])
-    print("Precio unitario: ", datos["productos"][4]["precio"])
-    print("Cantidad disponible: ", datos["productos"][4]["stock"])
-    print("\n")
-    print("Producto id: ", datos["productos"][5]["id"])
-    print("Descripción: ", datos["productos"][5]["nombre"])
-    print("Precio unitario: ", datos["productos"][5]["precio"])
-    print("Cantidad disponible: ", datos["productos"][5]["stock"])
-    print("\n")
-    print("Producto id: ", datos["productos"][6]["id"])
-    print("Descripción: ", datos["productos"][6]["nombre"])
-    print("Precio unitario: ", datos["productos"][6]["precio"])
-    print("Cantidad disponible: ", datos["productos"][6]["stock"])
-    print("\n")
-    print("Producto id: ", datos["productos"][8]["id"])
-    print("Descripción: ", datos["productos"][8]["nombre"])
-    print("Precio unitario: ", datos["productos"][8]["precio"])
-    print("Cantidad disponible: ", datos["productos"][8]["stock"])
-    print("\n")
-    print("Producto id: ", datos["productos"][9]["id"])
-    print("Descripción: ", datos["productos"][9]["nombre"])
-    print("Precio unitario: ", datos["productos"][9]["precio"])
-    print("Cantidad disponible: ", datos["productos"][9]["stock"])
-    print("\n")
-    print("Producto id: ", datos["productos"][10]["id"])
-    print("Descripción: ", datos["productos"][10]["nombre"])
-    print("Precio unitario: ", datos["productos"][10]["precio"])
-    print("Cantidad disponible: ", datos["productos"][10]["stock"])
-    print("\n")
-    print("Producto id: ", datos["productos"][11]["id"])
-    print("Descripción: ", datos["productos"][11]["nombre"])
-    print("Precio unitario: ", datos["productos"][11]["precio"])
-    print("Cantidad disponible: ", datos["productos"][11]["stock"])
-    print("\n")
-
-    #lista_1= [datos["productos"][0]["id"], datos["productos"][0]["nombre"], datos["productos"][0]["precio"], datos["productos"][0]["stock"]]
-    #print(lista_1)
+    for item in datos:
+        print(f"ID {item['id']}. {item['nombre']} -> Stock disponible:{item['stock']}")
+        print()
 
 def principal():
     datos_json= solicitar_stock(URL_API_GET)
@@ -87,11 +27,11 @@ def principal():
     if (datos_json == False):
         print("Error al solicitar datos")
     else:
-        mostrar_stock(datos_json)
+        mostrar_stock(datos_json["productos"])
+
 
 
 # HACER EL PEDIDO
-
 
 def enviar_pedido(a, b):
     parametros = {"id":a, "cantidad":b}
@@ -103,27 +43,30 @@ def enviar_pedido(a, b):
 
 
 def hacer_pedido():
+    pedido= []
+        
     while True:
-        print("Ingrese a continuación id del producto y cantidad, para finalizar el pedido presione '0' dos veces")
+        print("Ingrese a continuación id del producto y cantidad, para finalizar el pedido presione '0'")
         a= int(input("id del producto: "))
         b= int(input("cantidad: "))
-        
-        if ( a !=0 or b !=0 ):
-            proceso= enviar_pedido(a, b)
-            if (proceso == False):
-                print("Error al conectarse con el servidor")
-            else:
-                print(proceso["mensaje"])
+
+        if ( a !=0 ):
+            pedido.append({ "id": a, "cantidad": b })
         else:
-            print("Pedido finalizado")
             break
+
+    proceso= enviar_pedido(pedido[a], pedido[b])
+    if (proceso == True):
+            print("Error al conectarse con el servidor")
+    else:
+            print("Pedido finalizado")
+
             
 
 if (__name__ == "__main__"):
     principal()
     hacer_pedido()
 
-# pendientes: 1- acumular, 2- ver lo de los parámetros en otro archivo
 
 
 
